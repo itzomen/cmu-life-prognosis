0b9784898130206f54e9d00c75f0b3efbf7a2061
Peng Boris <peng@traleor.com>
2024-08-09 14:46:06 +0200

    Add update profile

--- scripts/export-patients.sh
+++ scripts/export-patients.sh
@@ -6,14 +6,14 @@
 OUTPUT_FILE="patients.csv"
 
 # Write the CSV header
-echo "UUID,Email,Role,Password,First Name,Last Name,Date of Birth,HIV Status,Diagnosis Date,ART Status,ART Date" > $OUTPUT_FILE
+echo "UUID,Email,Role,Password,First Name,Last Name,Date of Birth,Country,HIV Status,Diagnosis Date,ART Status,ART Date" > $OUTPUT_FILE
 
 # Read from user-store.txt and write patient details to the CSV file
-while IFS=: read -r uuid email role password fn ln dob hiv diagnosis_date art art_date; do
+while IFS=: read -r uuid email role password fn ln dob country hiv diagnosis_date art art_date; do
   # Only include patients in the CSV file
   if [ "$role" == "patient" ]; then
-    echo "$uuid,$email,$role,$password,$fn,$ln,$dob,$hiv,$diagnosis_date,$art,$art_date" >> $OUTPUT_FILE
+    echo "$uuid,$email,$role,$password,$fn,$ln,$dob,$country,$hiv,$diagnosis_date,$art,$art_date" >> $OUTPUT_FILE
   fi
 done < user-store.txt
 
-echo "$OUTPUT_FILE"
+echo "Exported patient data to $OUTPUT_FILE"
