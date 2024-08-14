77c5adfee907951583f8a2414e67f0be002693bc
Abel Alemu Jotie <ajotie@andrew.cmu.edu>
2024-08-14 10:26:59 +0200

    better UI

--- src/views/util/displayutil/Messages.java
+++ src/views/util/displayutil/Messages.java
@@ -1,16 +1,15 @@
 package views.util.displayutil;
 public class Messages{
   public static void getHelpMessage(){
-    System.out.println("   Use the numberings to choose the options \n" +
-    "   beware of input validations as names shouldn't contain number \n" +
-    "   Password should be at least a length of 8, contain upper and lowercase letters \n"+
-    "   UUID and email should be in a valid format \n" +
-            "   Use * to go back to previous options \n" +
-            "use $ to exit the program \n" +
-    "   Use exit options to stop the program \n" +
-    "   Date format is MM/dd/yyyy \n" +
-    "   "
+    System.out.println("-Use the numberings to choose the options \n" +
+    "-Beware of input validations as names shouldn't contain number \n" +
+    "-Password should be at least a length of 8, contain upper and lowercase letters \n"+
+    "-UUID and email should be in a valid format \n" +
+    "-Use * to go back to previous options \n" +
+    "-Use $ to exit the program \n" +
+    "-Use exit options to stop the program \n" +
+    "-Date format is MM/dd/yyyy \n" 
     );
   }
-  public static String invalidPassword="\"Invalid password(length: 8, numbers, uppercase and lowercase letters). Enter again or * to go back\"";
+  public static String invalidPassword="\"Invalid password(length: 8, numbers, uppercase and lowercase letters)\"";
 }
